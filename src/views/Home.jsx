fa399111c128017c1d864fe6be98968d41161635
BaskaranAathithyan <itsthiyan11@gmail.com>
2022-10-14 11:23:23 +0530

    Navbar

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import "../style/home.css";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <nav class="nav-menu">
+        <h2>Learning Management System</h2>
+        <ul class="menu">
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Courses</a>
+          </li>
+          <li>
+            <a href="#">Profile</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default Home;
